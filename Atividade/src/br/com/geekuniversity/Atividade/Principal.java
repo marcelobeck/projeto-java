385fe6c4788041cb34acf77dfbeb9474d5996b4d
marcelobeck <desenvolvimento4@brazilsistem.com.br>
2023-04-12 21:16:05 -0300

    Atividade

--- Atividade/src/br/com/geekuniversity/Atividade/Principal.java
+++ Atividade/src/br/com/geekuniversity/Atividade/Principal.java
@@ -5,6 +5,21 @@ import java.util.Scanner;
 public class Principal {
     public static void main(String[] args) {
 
+        double teste;
+        double valor;
+
+        Scanner scanner = new Scanner(System.in);
+        teste = scanner.nextInt();
+
+        if(teste > 0){
+            valor = Math.sqrt(teste);
+            System.out.println(valor);
+        } else {
+            valor = Math.pow(teste, 2);
+            System.out.println(valor);
+
+        }
+
        
     }
 }
